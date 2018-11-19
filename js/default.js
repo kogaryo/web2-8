6a4774285c2f52f7c48ef5cfe8ad62d6fd594bf6
kogaryo <38420497+kogaryo@users.noreply.github.com>
2018-11-19 17:37:39 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,3 +1,53 @@
+var separate_time = function(time)
+{
+    var sec =time.getSeconds();
+    var min =time.getMinutes();
+    var hours=time.getHours();
+    var days =time.getDate();
+    var month =time.getMonth();
+    var year =time.getFullYear();
+  return [sec,min,hours,days,month,year];
+}
+
+var update = function()
+{
+ var now = new Date();
+ var target = new Date(2020,7,24,0,0,0,0);
+var diff=target.getTime()-now.getTime();
+var counter = separate_time(diff);
+document.getElementById('countdown').textContent=
+  counter[5] + '年' +
+  counter[4] + '月' +
+  counter[3] + '日' +
+  counter[2] + '時' +
+  counter[1] + '分' +
+  counter[0] + '秒' +
+    refresh();
+}
+
+var refresh=function()
+{
+setTimeout(update,1000);
+}
+update();
+
+function getFileName(){
+ return window.location.href.split('/').pop();   
+}
+var filename = getFileName();
+var opt;
+if(filename === 'other.html'){
+    opt = document.querySelector('option[value="other.html"]');
+}else{
+ opt = document.querySelector('option[value="index.html"]');   
+}
+opt.selected=true;
+
+document.getElementById('form').select.onchange= functipon(){
+    location.href = document.getElementById('form').select.value;
+}
+
+
 function setCookie(c_name,value,expiredays){
  var extime = new Date().getTime();
  var cltime = new Date(extime + (60*60*24*1000*expiredays));
@@ -32,51 +82,12 @@ function getCookie(c_name){
   return "";
 }
 
-function getFileName(){
- return window.location.href.split('/').pop();   
-}
-var filename = getFileName();
-var opt;
-if(filename === 'other.html'){
-    opt = document.querySelector('option[value="other.html"]');
+var last_date = getCookie('lastDate');
+if(last_date){
+  document.getElementById('cookie').textContent = '前回訪れた時間:' +last_date;
 }else{
- opt = document.querySelector('option[value="index.html"]');   
-}
-opt.selected=true;
-
-document.getElementById('form').select.onchange= functipon(){
-    location.href = document.getElementById('form').select.value;
+ document.getElementById('cookie').textContent = 'はじめまして'; 
 }
 
-var separate_time = function(time)
-{
-    var sec =time.getSeconds();
-    var min =time.getMinutes();
-    var hours=time.getHours();
-    var days =time.getDate();
-    var month =time.getMonth();
-    var year =time.getFullYear();
-  return [sec,min,hours,days,month,year];
-}
-
-var update = function()
-{
- var now = new Date();
- var target = new Date(2020,7,24,0,0,0,0);
-var diff=target.getTime()-now.getTime();
-var counter = separate_time(diff);
-document.getElementById('countdown').textContent=
-  counter[5] + '年' +
-  counter[4] + '月' +
-  counter[3] + '日' +
-  counter[2] + '時' +
-  counter[1] + '分' +
-  counter[0] + '秒' +
-    refresh();
-}
-
-var refresh=function()
-{
-setTimeout(update,1000);
-}
-update();
+var current_time = new Date();
+setCookie('lastDate',current_time.toString(),7);
