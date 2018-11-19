068ca6a405c6624140c83e259a1d3638c83ecaeb
kogaryo <38420497+kogaryo@users.noreply.github.com>
2018-11-19 17:00:25 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -9,8 +9,12 @@ var separate_time = function(time)
   return [sec,min,hours,days,month,year];
 }
 
-var now =new Date();
-var counter = separate_time(now);
+var update = function()
+{
+ var now = new Date();
+ var target = new Date(2020,7,24,0,0,0,0);
+var diff=target.getTime()-now.getTime();
+var counter = separate_time(diff);
 document.getElementById('countdown').textContent=
   counter[5] + '年' +
   counter[4] + '月' +
@@ -18,3 +22,11 @@ document.getElementById('countdown').textContent=
   counter[2] + '時' +
   counter[1] + '分' +
   counter[0] + '秒' +
+    refresh();
+}
+
+var refresh=function()
+{
+setTimeout(update,1000);
+}
+update();
