151ffa35a658d78688883645946f1e313bc28c45
kogaryo <38420497+kogaryo@users.noreply.github.com>
2018-11-19 17:18:57 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,3 +1,19 @@
+function getFileName(){
+ return window.location.href.split('/').pop();   
+}
+var filename = getFileName();
+var opt;
+if(filename === 'other.html'){
+    opt = document.querySelector('option[value="other.html"]');
+}else{
+ opt = document.querySelector('option[value="index.html"]');   
+}
+opt.selected=true;
+
+document.getElementById('form').select.onchange= functipon(){
+    location.href = document.getElementById('form').select.value;
+}
+
 var separate_time = function(time)
 {
     var sec =time.getSeconds();
@@ -7,10 +23,6 @@ var separate_time = function(time)
     var month =time.getMonth();
     var year =time.getFullYear();
   return [sec,min,hours,days,month,year];
-}
-
-document.getElementById('form').select.onchange= functipon(){
-    location.href = document.getElementById('form').select.value;
 }
 
 var update = function()
